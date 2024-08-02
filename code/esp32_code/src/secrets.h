38c7295599296dd1361996f32d265e1d421a59c7
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-08-02 12:29:49 -0500

    inclusion of example of secrets.h

---
+++ code/esp32_code/src/secrets.h
@@ -0,0 +1,7 @@
+#define PLANT_NUMBER  "1101"                    // put your plant number
+#define THINGNAME "unthermal_" PLANT_NUMBER
+const char WIFI_SSID[] = "xxxxxxx";             // put the WiFi network
+const char WIFI_PASSWORD[] = "xxxxxx";          // put the WiFi password
+const char BROKER[] = "192.168.0.4";            // put the broker's address;
+const char USER[] = "unthermal";                // required if you define password authentication in your broker
+const char PASSWORD[] = "unthermal";            // required if you define password authentication in your broker
