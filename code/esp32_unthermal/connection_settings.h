c538ca4160eb001f9b9230a9cc55bbbca225cd82
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-08-13 17:18:43 -0500

    connection settings updated

--- code/esp32_unthermal/connection_settings.h
+++ code/esp32_unthermal/connection_settings.h
@@ -1,4 +1,4 @@
-#define PLANT_NUMBER  "PC-T-01"                 // set the number of your plant
+#define PLANT_NUMBER  "PC-T-06"                 // set the number of your plant
 #define THINGNAME "Thermal_" PLANT_NUMBER
 const char WIFI_SSID[] = "UNAL";             // set your WiFi network
 const char WIFI_PASSWORD[] = "UNAL";         // set the password of your WiFi network
