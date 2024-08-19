86ef0a2b9875b9c53bfd7916b49d3d6163cc3b8d
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-08-18 21:43:31 -0500

    revising layout canvas

--- code/python_code/unthermal/identsys.py
+++ code/python_code/unthermal/identsys.py
@@ -601,6 +601,7 @@ def step_open(system, yop=50, amplitude=5, t1=350, stab_time=89, uee_time=10):
     line_u, = uax.plot(t, u, color="#00d4aa")
     line_yt, = yax.plot(t, yt, color="#d40055")
     line_ut, = uax.plot(t, ut, color="#338000")
+    fig.canvas.layout.width = '100%'
     display_immediately(fig)
 
 
