ef8c434f21d1129dd96e2c7fe3ef3f651ebceefd
leonardo <lbermeoc@unal.edu.co>
2024-10-17 13:16:56 -0500

    files added to src structure

---
+++ code/esp32_unthermal/src/connection_settings.h
@@ -0,0 +1,11 @@
+#define PLANT_NUMBER  "TEMPER_DAVID"                 // set the number of your plant
+#define THINGNAME "Thermal_" PLANT_NUMBER
+const char WIFI_SSID[] = "Apto414-2";             // set your WiFi network
+const char WIFI_PASSWORD[] = "Sinergy_2096_2";         // set the password of your WiFi network
+const char BROKER[] = "18.204.70.207";         // set your broker address eg."192.168.1.100";
+
+// login access
+const char USER[] = "unthermal";                // do not change
+const char PASSWORD[] = "unthermal";            // do not change
+
+
