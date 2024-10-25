4716b1993a8e00bf002345b5a61cbd91498ff820
Leonardo Bermeo <lbermeoc@unal.edu.co>
2024-10-25 16:03:17 -0500

    settings corrected

--- code/esp32_unthermal/src/connection_settings.h
+++ code/esp32_unthermal/src/connection_settings.h
@@ -1,5 +1,5 @@
 // MQTT settings
-#define  PLANT_NUMBER  "PC-T_XX"                 // set the number of your plant
+#define  PLANT_NUMBER  "PC-T-XX"                 // set the number of your plant
 #define  THINGNAME "Thermal_" PLANT_NUMBER
 #define  BROKER  "192.168.1.100"         // set your broker address eg."192.168.1.100";
 #define  PORT     1883                  //1883
