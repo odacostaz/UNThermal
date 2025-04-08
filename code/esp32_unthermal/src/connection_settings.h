b34b0def3f5297e5b8012c6dcc51e3b57d071d6b
Leonardo Bermeo <lbermeoc@unal.edu.co>
2025-04-08 15:43:30 -0500

    firmware updated

--- code/esp32_unthermal/src/connection_settings.h
+++ code/esp32_unthermal/src/connection_settings.h
@@ -1,12 +1,12 @@
 // MQTT settings
-#define  PLANT_NUMBER  "THERMAL_LEO"                 // set the number of your plant
-#define  THINGNAME "Thermal_" PLANT_NUMBER
-#define  BROKER  "192.168.0.11"         // set your broker address eg."192.168.1.100";
+#define  PLANT_NUMBER  "PC-M-10"                 // set the number of your plant
+#define  THINGNAME "Motor_" PLANT_NUMBER
+#define  BROKER  "192.168.1.100"         // set your broker address eg."192.168.1.100";
 #define  PORT     1883                  //1883
 
 // WiFi Network
-const char WIFI_SSID[] = "Apto414-2";             // set your WiFi network
-const char WIFI_PASSWORD[] = "Sinergy_2096_2";    // set the password of your WiFi network
+const char WIFI_SSID[] = "lab_control";             // set your WiFi network
+const char WIFI_PASSWORD[] = "lab_control";    // set the password of your WiFi network
 
 // login access
 const char USER[] = "unthermal";                // do not change
